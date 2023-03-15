ef8552955343cff4ad3c768acbd85464cda71dfa
Bhjamin <benjamin@hancock1.com>
2023-03-14 23:29:36 -0600

    created slice

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,38 @@
 import { NavLink } from "react-router-dom"
+import { useState } from "react"
+import axios from "axios"
+import { setPotentialArticles } from "../redux/potentialArticles"
+import { useDispatch } from "react-redux"
+
+
+const { REACT_APP_API_KEY } = process.env
 
 const Header = () => {
+
+    const [input, setInput] = useState('')
+    let disptch = useDispatch()
+
+    const searchHandler = (e) => {
+        e.preventDefault()
+        axios.get(`https://newsapi.org/v2/everything?apiKey=${REACT_APP_API_KEY}&q=${input}&language=en`)
+        .them(res => {
+            console.log(res.data)
+            disptch(setPotentialArticles(res.data.articles))
+        })
+        .catch(err => console.log(err))
+    }
 
     return(
         <div>
             <NavLink to='/'>Home</NavLink>
             <NavLink to='/details'>Details</NavLink>
+
+
+            <form onSubmit={e => searchHandler(e)}>
+                <input placeholder="Search the news for something" onChange={e => setInput(e.target.value)} />
+                <button>Search</button>
+            </form>
+
         </div>
     )
  }
